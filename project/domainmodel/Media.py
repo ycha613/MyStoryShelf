feab1ab2a88199e5aa6ddd877c451a36dddc4705
ycha613 <ycha613@aucklanduni.ac.nz>
2025-04-23 22:29:13 +1200

    added book unit tests, changed domain model to have eq and hash defined in media subclasses not super class

--- project/domainmodel/Media.py
+++ project/domainmodel/Media.py
@@ -1,12 +1,28 @@
 from __future__ import annotations
-import project.domainmodel.User as User
+from project.domainmodel.User import User
+
+
+def validate_string(s: str):
+    if not isinstance(s, str):
+        raise TypeError("This field must be a string.")
+    if len(s.strip()) <= 0:
+        raise ValueError("The field must be non empty.")
+    
+def validate_positive_int(i: int):
+    if not isinstance(i, int):
+        raise TypeError("This field must be a integer.")
+    if i <= 0:
+        raise ValueError("This field must be a positive integer.")
 
 
 class Media:
-    def __init__(self, title: str, user: User.User):
+    def __init__(self, title: str, user: User):
         self._id = None
+        validate_string(title)
         adj_title = " ".join([a.capitalize() for a in title.strip().split()])
         self._title = adj_title
+        if not isinstance(user, User):
+            raise TypeError("User must be a User object.")
         self._user = user
 
     @property
@@ -18,55 +34,76 @@ class Media:
         return self._title
     
     @property
-    def user(self) -> User.User:
+    def user(self) -> User:
         return self._user
     
     def __repr__(self) -> str:
         return f"<{self.__class__.__name__}: {self.title}>"
-    
-    def __eq__(self, other) -> bool:
-        if not isinstance(other, self.__class__):
-            return False
-        else:
-            return self._title.lower() == other._title.lower()
         
     def __lt__(self, other) -> bool:
         if not isinstance(other, Media):
             return False
         else:
-            return self._title.lower() < other._title.lower()
-        
-    def __hash__(self) -> int:
-        return hash(self._title.lower())
+            return self._title < other._title
 
 
 class Book(Media):
-    def __init__(self, title: str, user: User.User, author: str):
+    def __init__(self, title: str, user: User, author: str):
         super().__init__(title, user)
+        validate_string(author)
         adj_author = " ".join([a.capitalize() for a in author.strip().split()])
         self._author = adj_author
 
     @property
     def author(self) -> str:
         return self._author
+    
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Book):
+            return False
+        else:
+            return self._title == other._title and self._author == other._author
+        
+    def __hash__(self) -> int:
+        return hash(self._title + self._author + self.__class__.__name__)
 
    
 class Movie(Media):
-    def __init__(self, title: str, user: User.User, director: str):
+    def __init__(self, title: str, user: User, director: str):
         super().__init__(title, user)
+        validate_string(director)
         adj_director = " ".join([a.capitalize() for a in director.strip().split()])
         self._director = adj_director
 
     @property
     def director(self) -> str:
         return self._director
+    
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Movie):
+            return False
+        else:
+            return self._title == other._title and self._director == other._director
+    
+    def __hash__(self) -> int:
+        return hash(self._title + self._director + self.__class__.__name__)
 
 
 class Show(Media):
-    def __init__(self, title: str, user: User.User, season: int):
+    def __init__(self, title: str, user: User, season: int):
         super().__init__(title, user)
+        validate_positive_int(season)
         self._season = season
     
     @property
     def season(self) -> int:
         return self._season
+    
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Show):
+            return False
+        else:
+            return self._title == other._title and self._season == other._season
+        
+    def __hash__(self) -> int:
+        return hash(self._title + str(self._season) + self.__class__.__name__)
--- tests/unit/test_domainmodel.py
+++ tests/unit/test_domainmodel.py
@@ -1,5 +1,6 @@
 import pytest
 from project.domainmodel.User import User
+from project.domainmodel.Media import Book, Movie, Show
 
 """
 Unit tests for the domain model of the web application
@@ -53,8 +54,63 @@ def test_user_hash():
     user_set.discard(user1)
     assert repr(sorted(user_set)) == "[<User: brian>, <User: chris>]"
 
+
 # Book class unit tests
 
+def test_book_initialisation(my_user):
+    book1 = Book(title="Moby Dick", user=my_user, author="Herman Melville")
+    assert repr(book1) == "<Book: Moby Dick>"
+    assert book1.title == "Moby Dick"
+    assert book1.author == "Herman Melville"
+
+    with pytest.raises(TypeError):
+        book2 = Book(title=3, user=my_user, author="Herman Melville")
+    with pytest.raises(TypeError):
+        book2 = Book(title="Moby Dick 2", user=2, author="Herman Melville")
+    with pytest.raises(TypeError):
+        book2 = Book(title="Moby Dick 2", user=my_user, author=2)
+
+    with pytest.raises(ValueError):
+        book2 = Book(title="", user=my_user, author="Herman Melville")
+    with pytest.raises(ValueError):
+        book2 = Book(title="Moby Dick 2", user=my_user, author="")
+
+    book2 = Book(title="moby DICK", user=my_user, author="herman MELVILLE")
+    assert book2.title == "Moby Dick"
+    assert book2.author == "Herman Melville"
+
+def test_book_eq(my_user):
+    book1 = Book(title="Moby Dick", user=my_user, author="Herman Melville")
+    book2 = Book(title="Moby Dick", user=my_user, author="Herman Melville")
+    book3 = Book(title="Neverland", user=my_user, author="Herman Melville")
+    book4 = Book(title="Moby Dick", user=my_user, author="John Richards")
+    assert book1 == book2
+    assert book1 != book3
+    assert book1 != book4
+    assert book4 == book4
+
+def test_book_lt(my_user):
+    book1 = Book(title="Moby Dick", user=my_user, author="Herman Melville")
+    book2 = Book(title="Neverland", user=my_user, author="Herman Melville")
+    book3 = Book(title="Orpheus", user=my_user, author="Herman Melville")
+    books_list = [book3, book2, book1]
+    assert book1 < book2 < book3
+    assert sorted(books_list) == [book1, book2, book3]
+
+
+def test_book_hash(my_user):
+    book1 = Book(title="Moby Dick", user=my_user, author="Herman Melville")
+    book2 = Book(title="Neverland", user=my_user, author="Herman Melville")
+    book3 = Book(title="Orpheus", user=my_user, author="Herman Melville")
+    books_set = set()
+    books_set.add(book1)
+    books_set.add(book2)
+    books_set.add(book3)
+
+    assert len(books_set) == 3
+    assert repr(sorted(books_set)) == "[<Book: Moby Dick>, <Book: Neverland>, <Book: Orpheus>]"
+    books_set.discard(book1)
+    assert repr(sorted(books_set)) == "[<Book: Neverland>, <Book: Orpheus>]"
 
 # Movie class unit tests
 
